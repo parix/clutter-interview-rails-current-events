50d4e6fc6bb38faff1812403ab4fdd38c2ab8071
Chris or Allen Rosario <c.allen.rosario@gmail.com>
2018-01-13 19:21:59 -0800

    Added Bootstrap UI.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,17 @@
 class EventsController < ApplicationController
+  def index
+    @event_dates = EventDate.all
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def show
+    @event_date = EventDate.find_by(date: Date.parse(params[:date]))
+
+    respond_to do |format|
+      format.html
+    end
+  end
 end
--- lib/wiki_scraper/lib/wiki_scraper/article.rb
+++ lib/wiki_scraper/lib/wiki_scraper/article.rb
@@ -6,8 +6,11 @@ class WikiScraper
     attr_accessor :url, :doc
 
     def initialize(url)
-      @url = url
-      @doc = Nokogiri::HTML(open(url))
+      @url = URI(url)
+    end
+
+    def doc
+      @doc ||= Nokogiri::HTML(open(url))
     end
 
     def self.get_summary(url)
@@ -15,27 +18,34 @@ class WikiScraper
     end
 
     def get_summary
-      { "title" => get_title,
-        "summary" => get_summary_text,
-        "image_url" => get_image_url }
+      if @url.host.include?("wikipedia")
+        { "title" => get_title,
+          "summary" => get_summary_text,
+          "image_url" => get_image_url }
+      end
     end
 
     def get_title
-      @doc.css("h1[id='firstHeading']").text
+      doc.css("h1[id='firstHeading']").text
     end
 
     def get_summary_text
       summary = []
-      children = @doc.css("div[class='mw-parser-output']").children
-      children.each { |c| break if c.name == 'div' && c.attr('id') == 'toc'; summary << c.to_html if c.name == 'p'; }
+      children = doc.css("div[class='mw-parser-output']").children
+      children.each do |c|
+        break if c.attr('id') == 'toc' || c.xpath(".//*[contains(@id, 'toc')]").count > 0
+        summary << c.to_html if c.name == 'p'
+      end
       summary = summary.join
       summary.gsub!('<a href="/', '<a href="https://en.wikipedia.org/wiki')
       summary.gsub!('<a href="#', "<a href=\"#{@url}#")
+      summary
+      #URI.join("https://en.wikipedia.org"
     end
 
     def get_image_url
-      if img = @doc.xpath("//div[@class='mw-parser-output']//img").first
-        img.attr('src').gsub(/^\/+/,'')
+      if img = doc.xpath("//div[@class='mw-parser-output']//img").first
+        img.attr('src')
       else
         nil
       end
